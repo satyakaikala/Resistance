10617842cabb075234c6ffa93171bb4f32850aed
satyakaikla <satya.kaikala.786@gmail.com>
2016-05-13 00:30:23 -0400

    class to compare cell value with neighbor cells.

--- src/resistance/Constant.java
+++ src/resistance/Constant.java
@@ -1,5 +1,6 @@
 package resistance;
 
 public class Constant {
-
+	public static String DELIMITER = ",";
+	public static int ZERO = 0;
 }
--- src/resistance/Data.java
+++ src/resistance/Data.java
@@ -1,5 +1,45 @@
 package resistance;
 
-public class Data {
+public class Data implements Comparable<Data> {
+	private String pathValues;
+	private int totalValue;
+
+	public Data(String pathValues, int totalValue) {
+		super();
+		this.pathValues = pathValues.substring(Constant.ZERO, pathValues.length() - 1);
+		this.totalValue = totalValue;
+	}
+
+	public String getPathValues() {
+		return pathValues;
+	}
+
+	public void setPathValues(String pathValues) {
+		this.pathValues = pathValues;
+	}
+
+	public int getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(int totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	@Override
+	public String toString() {
+		return "Data [pathValues=" + pathValues + ", totalValue=" + totalValue + "]";
+	}
+
+	@Override
+	public int compareTo(Data d) {
+		if (this.totalValue == d.getTotalValue()) {
+			return 0;
+		} else if (this.totalValue > d.getTotalValue()) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
 
 }
